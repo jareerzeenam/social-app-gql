6938a422f69633b0b18cc4f296c04bb3ad7beef8
jareer <jareerzeenam@adventus.io>
2022-01-03 16:16:39 +0530

    Create Post validation added

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -34,7 +34,10 @@ module.exports = {
     //Create Post
     async createPost(_, { body }, context) {
       const user = checkAuth(context);
-      console.log(user);
+
+      if (args.body.trim() === '') {
+        throw new Error('Post body cannot be empty');
+      }
 
       const newPost = new Post({
         body,
